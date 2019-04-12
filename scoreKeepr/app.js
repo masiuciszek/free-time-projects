ae3df2875172564df1d5852461231f68be685b00
Marcell Ciszek Druzynski <40737996+masiuciszek@users.noreply.github.com>
2019-04-12 09:00:19 +0200

    Add files via upload

---
+++ scoreKeepr/app.js
@@ -0,0 +1,54 @@
+const p1Button = document.querySelector("#p1");
+const p2Button = document.getElementById("p2");
+const resetButton = document.getElementById("reset");
+const p1Display = document.querySelector("#p1Display");
+const p2Display = document.querySelector("#p2Display");
+const numInput = document.querySelector("input");
+const winningScoreDisplay = document.querySelector("p span");
+let p1Score = 0;
+let p2Score = 0;
+let gameOver = false;
+let winningScore = 10;
+
+p1Button.addEventListener('click', () => {
+    if(!gameOver){
+        p1Score++;
+        if(p1Score === winningScore){
+			p1Display.classList.add("winner");
+			gameOver = true;
+		}
+		p1Display.textContent = p1Score;
+	}
+});
+
+p2Button.addEventListener("click",() =>{
+	if(!gameOver){
+		p2Score++;
+		if(p2Score === winningScore){
+			p2Display.classList.add("winner");
+			gameOver = true;
+		}
+		p2Display.textContent = p2Score;
+	}
+});
+
+resetButton.addEventListener("click", () => {
+	reset();
+});
+
+function reset(){
+	p1Score = 0;
+	p2Score = 0;
+	p1Display.textContent = 0;
+	p2Display.textContent = 0;
+	p1Display.classList.remove("winner");
+	p2Display.classList.remove("winner");
+	gameOver = false;
+}
+
+numInput.addEventListener("change", function(){
+	winningScoreDisplay.textContent = this.value;
+	winningScore = Number(this.value);
+	reset();
+});
+    
